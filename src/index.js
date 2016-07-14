b856f2e5bb700a1568ae91df805515860c15e650
Matthew Goldstein <matthew.s.goldstein.18@dartmouth.edu>
2016-07-14 14:08:21 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-// es6 import style
-import $ from 'jquery';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
 
-import './style.scss';
-
-
-// this is a generator function.
-// note the *
-function *simpleCounter() {
-  let count = 0;
-  while (true) {
-    yield count++;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    youtubeSearch('pixar').then(videos => {
+      console.log(videos);
+    });
   }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+
 }
 
-// instantiate generator
-const counter = simpleCounter();
-
-setInterval(
-  // arrow notation for anonymous function
-  () => { $('#main').html(`You've been on this page for ${counter.next().value} seconds.`); }
-  , 1000
-);
+ReactDOM.render(<App />, document.getElementById('main'));
